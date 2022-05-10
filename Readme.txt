fbb82a3c473637de6caac5ee05b1d4e9bd895276
Ankita Sharma <60ankitasharma@gmail.com>
2022-05-10 22:54:07 +0530

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,27 @@
+How to run Project
+1. Download and Unzip the file on your local system copy carrental .
+2. Put carrental folder inside root directory (for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/html)
+
+Database Configuration
+
+Open phpmyadmin
+Create Database carrental
+Import database carrental.sql (available SQL File Folder inside zip package)
+
+For User
+Open Your browser put inside browser “http://localhost/carrental”
+Login Details for user:
+Email : soni@gmail.com
+Password: 123
+
+For Admin Panel
+Open Your browser put inside browser “http://localhost/carrental/admin”
+Login Details for admin :
+Username: admin
+Password: 1234
+
+This project is free to use both personal and commercial
+
+For commercial use, we recommend to first contact us to give support, improve on security and to add more useful functionalities
+
+All our services are free don't fear to contact us on 60ankitasharma@gmail.com
---
+++ about.php
@@ -0,0 +1,91 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Car Rental | About Us</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <meta content="Author" name="WebThemez">
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet"> 
+</head>
+
+<body id="body"> 
+ <?php include('includes/header.php');?>
+ <section id="innerBanner"> 
+  <div class="inner-content">
+    <h2><span>About Us</span><br>We provide high quality cars</h2>
+    <div> 
+    </div>
+  </div> 
+</section><!-- #Page Banner -->
+
+<main id="main">
+      <section id="about" class="wow fadeInUp">
+        <div class="container"> 
+          <div class="row">
+            <div class="col-lg-6 about-img">
+              <img src="img/nissan.jpg" alt="">
+            </div>
+
+            <div class="col-lg-6 content">
+              <h2>One of the finest people mobility companies in India with separate divisions serving Corporate executive car rental and employee transportation, serving travel agents, tour operators and Event management companies for their ground transportation needs.</h2>
+              <h3>CarRental offers Chauffeur Driven from mid to luxury segment cars ranging with brands like Tata, Volvo, Imported Mercedes and Toyota Coaches with the only company offering Chrysler Stretch Limousines.</h3>
+              <p>With a professional management and offices in Delhi, Noida, Gurgaon, Jaipur, Mumbai, Pune, Kolkata, Ahmedabad, Hyderabad, Chennai, Bangalore, Cochin, Goa, Coimbatore, Lucknow and 80 other cities in India, CarRental is now probably India’s largest B2B passenger transportation company.  Our partnership with CarRental gives our clients access to the best cars and privileges at over 3700 locations in over 130 countries worldwide</p>
+              <ul>
+                <li><i class="icon ion-ios-checkmark-outline"></i> CarRental has always been a leader in tech with in house technology driving every aspect of the business to offer a seamless experience to our customers.</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i>Thanks to all our loyal clients and team members who have stood by us over the years, the brand has been built on operational excellence and repeated customers.</li>
+                
+              </ul> 
+            </div>
+          </div>
+
+        </div>
+      </section><!-- #about -->
+    </main>
+    <?php include('includes/footer.php');?>
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- JavaScript  -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+    <script src="lib/sticky/sticky.js"></script> 
+    <script src="contact/jqBootstrapValidation.js"></script>
+    <script src="contact/contact_me.js"></script>
+    <script src="js/main.js"></script>
+
+  </body>
+  </html>
